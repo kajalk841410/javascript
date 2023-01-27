329239f573806d91d19099ab4a8c4676b6ace066
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2023-01-27 14:25:49 +0530

    Create display time and date.js

---
+++ display time and date.js
@@ -0,0 +1,11 @@
+<! DOCTYPE html>
+<html>
+<body>
+<h2> My first Javascript</h2>
+<button type="button"
+onclick="document.getElementById('demo').innerHTML =Date()">
+click me to display date and Time.</button>
+<p id="demo">     </p>
+
+</body>
+</html>
