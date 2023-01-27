5701550f82b974920afbf331c70e06c7a00cc277
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2023-01-27 14:44:59 +0530

    Create change css.js

---
+++ change css.js
@@ -0,0 +1,10 @@
+<! DOCTYPE html>
+<html>
+<body>
+<h2>Change CSS</h2>
+
+<p id="demo">JavaScript can change the style of an HTML element.</p>
+
+<button type="button" onclick="document.getElementById('demo').style.fontSize='45px'">Click Me!</button>
+</body>
+</html>
